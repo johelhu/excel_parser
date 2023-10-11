250d775d722e1a62a8767c1f850dc1fc868ee567
Johel Hidalgo <johelhu@gmail.com>
2023-10-10 18:05:28 -0600

    Prueba de programa

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import argparse
-from sys import exit
+import openpyxl
 
 parser = argparse.ArgumentParser()
 
@@ -12,19 +12,41 @@ parser.add_argument(
 
 parser.add_argument(
     '-c', '--code',
-    help='codigo del artefacto',
+    help='ID, apellido o nombre del paciente a buscar',
     required=True,
     type=str,
 )
 
-
 args = parser.parse_args()
 
-if not '.xlsx' in args.excel_file:
-    print("Mal formato")
+if not args.excel_file.endswith('.xlsx'):
+    print("Formato de archivo no válido. Se esperaba un archivo con extensión .xlsx")
     exit(1)
 
-# Empieza el script
+print("Archivo Excel:", args.excel_file)
+print("Código, apellido o nombre del paciente a buscar:", args.code)
 
-print(args.excel_file)
-print(args.code)
+workbook = openpyxl.load_workbook(args.excel_file)
+sheet = workbook.active
+
+def buscar_paciente(codigo):
+    for row in sheet.iter_rows(min_row=2):
+        print("Leyendo fila:", [cell.value for cell in row])
+        if len(row) >= 3 and (
+                (row[0].value and str(row[0].value).lower() == str(codigo)) or
+                (row[1].value and str(row[1].value).lower() == str(codigo)) or
+                (row[2].value and str(row[2].value) == str(codigo))):
+            return row
+
+    return None
+
+print(buscar_paciente(args.code))
+resultado = buscar_paciente(args.code)
+
+
+if resultado:
+    nombre_paciente = resultado[0].value
+    apellido_paciente = resultado[1].value
+    print('Paciente encontrado - Nombre:', nombre_paciente, ', Apellido:', apellido_paciente, ', ID:', args.code)
+else:
+    print('Paciente no encontrado.')
