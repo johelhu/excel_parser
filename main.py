798662af4b5e40bac3f23b4eeab2aa7e2c43626d
Johel Hidalgo <johelhu@gmail.com>
2023-10-11 17:35:41 -0600

    Funcion de busqueda de archivo excel, agregar pandas y .gitignore

--- main.py
+++ main.py
@@ -1,52 +1,25 @@
 import argparse
 import openpyxl
 
-parser = argparse.ArgumentParser()
+from administradorDatos import inicializar_datos 
 
-parser.add_argument(
-    '-e', '--excel-file',
-    help='nombre del archivo Excel',
-    required=True,
-    type=str,
-)
+#parser = argparse.ArgumentParser()
 
-parser.add_argument(
-    '-c', '--code',
-    help='ID, apellido o nombre del paciente a buscar',
-    required=True,
-    type=str,
-)
+#parser.add_argument(
+ #   '-e', '--excel-file',
+  #  help='nombre del archivo Excel',
+   # required=True,
+    #type=str,
+#)
 
-args = parser.parse_args()
+#parser.add_argument(
+ #   '-c', '--code',
+  #  help='ID, apellido o nombre del paciente a buscar',
+   # required=True,
+    #type=str,
+#)
 
-if not args.excel_file.endswith('.xlsx'):
-    print("Formato de archivo no válido. Se esperaba un archivo con extensión .xlsx")
-    exit(1)
+#args = parser.parse_args()
 
-print("Archivo Excel:", args.excel_file)
-print("Código, apellido o nombre del paciente a buscar:", args.code)
-
-workbook = openpyxl.load_workbook(args.excel_file)
-sheet = workbook.active
-
-def buscar_paciente(codigo):
-    for row in sheet.iter_rows(min_row=2):
-        print("Leyendo fila:", [cell.value for cell in row])
-        if len(row) >= 3 and (
-                (row[0].value and str(row[0].value).lower() == str(codigo)) or
-                (row[1].value and str(row[1].value).lower() == str(codigo)) or
-                (row[2].value and str(row[2].value) == str(codigo))):
-            return row
-
-    return None
-
-print(buscar_paciente(args.code))
-resultado = buscar_paciente(args.code)
-
-
-if resultado:
-    nombre_paciente = resultado[0].value
-    apellido_paciente = resultado[1].value
-    print('Paciente encontrado - Nombre:', nombre_paciente, ', Apellido:', apellido_paciente, ', ID:', args.code)
-else:
-    print('Paciente no encontrado.')
+df = inicializar_datos("pacientes.xlsx") # Sacamos la variable df de la funcion inicializar_datos
+print(df)
