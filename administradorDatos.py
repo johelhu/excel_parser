27fd2e953dbc596323c08977388e4dca42a7a340
Johel Hidalgo <johelhu@gmail.com>
2023-10-15 20:09:06 -0600

    prototipo 1 de pasar datos de una hoja de excel a otra

--- administradorDatos.py
+++ administradorDatos.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python 
 
+import openpyxl
 import pandas as pd # importar libreria pandas, se usa para interactuar con tablas de datos
 
 def inicializar_datos(archivo): # inicia la funcion 
     try: # intenta hacer
-        # lee el excel llamado archivo, y convierte el card_uid en un str, todo lo guarda en dataframe(df)
-        df = pd.read_excel(archivo, converters={'ID': str}) 
+        # lee el excel llamado archivo, y convierte el ID en un str, todo lo guarda en dataframe(df)
+        df = pd.read_excel(archivo, converters={'id_paciente': str}) 
         print('Se encontró el archivo de datos')
 
     except FileNotFoundError: # excepto si 
@@ -14,3 +15,49 @@ def inicializar_datos(archivo): # inicia la funcion
         exit(1)
 
     return df # se retorna el valor y acaba la función en este caso nos guarda que que guardamos en df para usarlo en otro lugar
+
+def inicializar_hojas(archivo):
+    # Cargar el archivo Excel
+    workbook = openpyxl.load_workbook('pacientes.xlsx')
+    # Obtener los nombres de las hojas
+    print("Nombres de las hojas en el archivo:")
+    for sheet_name in workbook.sheetnames:
+        print(sheet_name)
+        
+def mover_datos(archivo, id_paciente, origen, destino):
+    # Cargar el archivo Excel
+    workbook = openpyxl.load_workbook(archivo)
+
+    # Determinar las hojas de origen y destino
+    sheet_origen = workbook[origen]
+    sheet_destino = workbook[destino]
+
+    # Imprimir el ID que estamos buscando
+    print("ID a buscar:", id_paciente)
+
+    # Imprimir los IDs en la hoja de origen para verificar
+    print("IDs en la hoja de origen:")
+    for fila in sheet_origen.iter_rows(min_row=2, max_row=sheet_origen.max_row, max_col=1, values_only=True):
+        print(fila[0])
+
+    # Encontrar la fila con el ID especificado
+    fila = 2  # Empezamos desde la segunda fila
+    while str(sheet_origen.cell(row=fila, column=3).value) != id_paciente:
+        fila += 1
+
+    # Obtener los datos del paciente
+    datos_paciente = [cell.value for cell in sheet_origen[fila]]
+
+    # Encontrar la última fila vacía en la hoja de destino
+    ultima_fila = sheet_destino.max_row + 1
+
+    # Escribir los datos en la última fila vacía de la hoja de destino
+    for i, valor in enumerate(datos_paciente):
+        sheet_destino.cell(row=ultima_fila, column=i+1).value = valor
+
+    # Eliminar los datos de la hoja de origen
+    for cell in sheet_origen[fila]:
+        cell.value = None
+
+    # Guardar el archivo Excel
+    workbook.save(archivo)
--- main.py
+++ main.py
@@ -1,25 +1,23 @@
+#!/usr/bin/env python 
+
 import argparse
 import openpyxl
 
-from administradorDatos import inicializar_datos 
-
-#parser = argparse.ArgumentParser()
-
-#parser.add_argument(
- #   '-e', '--excel-file',
-  #  help='nombre del archivo Excel',
-   # required=True,
-    #type=str,
-#)
-
-#parser.add_argument(
- #   '-c', '--code',
-  #  help='ID, apellido o nombre del paciente a buscar',
-   # required=True,
-    #type=str,
-#)
-
-#args = parser.parse_args()
+from administradorDatos import inicializar_datos,inicializar_hojas, mover_datos
 
 df = inicializar_datos("pacientes.xlsx") # Sacamos la variable df de la funcion inicializar_datos
 print(df)
+
+# Argumentos del programa
+parser = argparse.ArgumentParser(description='Mover datos entre hojas en el archivo de pacientes')
+parser.add_argument('-e', '--excel-file', help='nombre del archivo Excel', required=True, type=str)
+parser.add_argument('-i', '--id_paciente', help='ID del paciente para identificar la fila', required=True, type=str)
+parser.add_argument('-o', '--origen', choices=['Pendientes', 'Terminados'], required=True, help='Hoja de origen')
+parser.add_argument('-d', '--destino', choices=['Pendientes', 'Terminados'], required=True, help='Hoja de destino')
+args = parser.parse_args()
+
+# Inicializar las hojas
+inicializar_hojas(args.excel_file)
+
+# Llamar a la función para mover los datos
+mover_datos(args.excel_file, args.id_paciente, args.origen, args.destino)
